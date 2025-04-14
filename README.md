cb79cd2e3e7802f8ad1b7147c11345ffba9a59ae
Lee <7932644+strahe@users.noreply.github.com>
2025-04-14 14:29:52 +0800

    Fix Cloudflare naming and use remote-mcp

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cloudflare-context-server
 Zed Cloudflare Context Server
+
+## Usage
+
+When using for the first time, an authorization page will be opened in the browser, log in to the cloudflare account and agree to authorize.
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,14 @@
-use serde::Deserialize;
 use std::env;
-use zed::settings::ContextServerSettings;
-use zed_extension_api::{self as zed, serde_json, ContextServerId, Result};
+use zed_extension_api::{self as zed, ContextServerId, Result};
 
-const PACKAGE_NAME: &str = "@cloudflare/mcp-server-cloudflare";
-const PACKAGE_VERSION: &str = "0.2.0";
-const SERVER_PATH: &str = "node_modules/@cloudflare/mcp-server-cloudflare/dist/index.js";
+const PACKAGE_NAME: &str = "mcp-remote";
+const PACKAGE_VERSION: &str = "0.0.20";
+const SERVER_PATH: &str = "node_modules/mcp-remote/dist/client.js";
+const SERVER_ADDRESS: &str = "https://mcp.cloudflare.com/workers/observability/sse";
 
-struct CloudfareModelContextServer;
+struct CloudflareModelContextServer;
 
-#[derive(Debug, Deserialize)]
-struct CloudfareModelContextServerSettings {
-    cloudflare_account_id: String,
-}
-
-impl zed::Extension for CloudfareModelContextServer {
+impl zed::Extension for CloudflareModelContextServer {
     fn new() -> Self {
         Self
     }
@@ -22,43 +16,26 @@ impl zed::Extension for CloudfareModelContextServer {
     fn context_server_command(
         &mut self,
         _context_server_id: &ContextServerId,
-        project: &zed::Project,
+        _project: &zed::Project,
     ) -> Result<zed::Command> {
         let version = zed::npm_package_installed_version(PACKAGE_NAME)?;
         if version.as_deref() != Some(PACKAGE_VERSION) {
             zed::npm_install_package(PACKAGE_NAME, PACKAGE_VERSION)?;
         }
 
-        let ctx_server_settings =
-            ContextServerSettings::for_project("cloudfare-context-server", project)?;
-
-        let Some(settings) = ctx_server_settings.settings else {
-            Err("missing settings for cloudfare-context-server")?
-        };
-        let settings: CloudfareModelContextServerSettings =
-            serde_json::from_value(settings).map_err(|e| e.to_string())?;
-
-        if settings.cloudflare_account_id.is_empty() {
-            Err("missing cloudflare_account_id in cloudfare-context-server settings")?;
-        }
-
         Ok(zed::Command {
-            command: "node".to_string(),
+            command: zed::node_binary_path()?,
             args: vec![
                 env::current_dir()
                     .unwrap()
                     .join(SERVER_PATH)
                     .to_string_lossy()
                     .to_string(),
-                "run".to_string(),
-                settings.cloudflare_account_id.to_string(),
+                SERVER_ADDRESS.to_string(),
             ],
-            env: vec![(
-                "CLOUDFLARE_ACCOUNT_ID".into(),
-                settings.cloudflare_account_id,
-            )],
+            env: vec![],
         })
     }
 }
 
-zed::register_extension!(CloudfareModelContextServer);
+zed::register_extension!(CloudflareModelContextServer);
